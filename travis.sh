976fbc90ddb7a37c726984c6cae31d99c350f945
David Lawrence <dkl@mozilla.com>
2014-05-07 16:11:50 +0000

    Bug 983275 - Switch Bugzilla's CI testing from Tinderbox to travis-ci

--- travis.sh
+++ travis.sh
@@ -6,15 +6,12 @@
 # This Source Code Form is "Incompatible With Secondary Licenses", as
 # defined by the Mozilla Public License, v. 2.0.
 
-QA_REPO="dklawren/qa"
-QA_BRANCH="$TRAVIS_BRANCH"
-
 # Setup starts here
 cd $TRAVIS_BUILD_DIR
 
 # Package installation section
 echo "== Installing OS packages"
-sudo apt-get install -y perlmagick apache2 libssl-dev g++ libgd2-xpm-dev xvfb
+sudo apt-get install -y curl perlmagick libssl-dev g++ libgd2-xpm-dev libmysqlclient-dev libpq5 postgresql-server-dev-9.1
 
 if [ "$TEST_SUITE" = "docs" ]; then
     if [ "$TRAVIS_BRANCH" = "master" ]; then
@@ -36,16 +33,25 @@ fi
 echo "== Installing Perl dependencies"
 cpanm --quiet --notest --reinstall DateTime
 cpanm --quiet --notest --reinstall Module::Build # Need latest build
-cpanm --quiet --notest --reinstall Software::License # Needed by Module::Build
+cpanm --quiet --notest --reinstall Software::License # Needed by Module::Build to find proper Mozilla license
 cpanm --quiet --notest --reinstall Pod::Coverage
+cpanm --quiet --notest --reinstall DBD::mysql
+cpanm --quiet --notest --reinstall DBD::Pg
+cpanm --quiet --notest --reinstall Cache::Memcached::GetParserXS # FIXME test-checksetup.pl fails without this
 cpanm --quiet --notest --installdeps --with-recommends .
+
+# Link /usr/bin/perl to the current perlbrew perl so that the Bugzilla cgi scripts will work properly
+# PERLBREW_ROOT and PERLBREW_PERL are set by the perlbrew binary when switch perl versions
+echo "== Fixing Perl"
+sudo mv /usr/bin/perl /usr/bin/perl.bak
+sudo ln -s $PERLBREW_ROOT/perls/$PERLBREW_PERL/bin/perl /usr/bin/perl
 
 # Basic sanity tests
 if [ "$TEST_SUITE" = "sanity" ]; then
     echo "== Running sanity tests"
     perl Build.PL
-    ./Build
-    ./Build test
+    perl Build
+    perl Build test
     exit $?
 fi
 
@@ -72,6 +78,31 @@ fi
 echo "== Updating config files"
 sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place qa/config/selenium_test.conf
 sed -e "s?%USER%?$USER?g" --in-place qa/config/checksetup_answers.txt
+sed -e "s?%USER%?$USER?g" --in-place qa/config/checksetup_upgrade_answers.txt
+sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place qa/config/config-checksetup-mysql
+
+# Create the bugs user for MySQL
+if [ "$DB" = "mysql" ]; then
+    mysql -u root mysql -e "GRANT ALL PRIVILEGES ON *.* TO bugs@localhost IDENTIFIED BY 'bugs'; FLUSH PRIVILEGES;"
+fi
+
+# Create the bugs user for PostgreSQL
+if [ "$DB" = "pg" ]; then
+    sudo -u postgres createuser --superuser bugs
+    sudo -u postgres psql -U postgres -d postgres -c "alter user bugs with password 'bugs';"
+fi
+
+if [ "$TEST_SUITE" = "checksetup" ] && [ "$DB" = "mysql" ]; then
+    echo "== Running checksetup tests for MySQL"
+    perl qa/test-checksetup.pl --full --config config/config-checksetup-mysql
+    exit $?
+fi
+
+if [ "$TEST_SUITE" = "checksetup" ] && [ "$DB" = "pg" ]; then
+    echo "== Running checksetup tests for PostgreSQL"
+    perl qa/test-checksetup.pl --full --config config/config-checksetup-pg
+    exit $?
+fi
 
 # Configure Apache to serve from our build directory and restart
 sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place qa/config/bugzilla.conf
@@ -79,15 +110,6 @@ sudo cp qa/config/bugzilla.conf /etc/apache2/sites-available/default
 sudo sed -e "s?APACHE_RUN_USER=www-data?APACHE_RUN_USER=$USER?g" --in-place /etc/apache2/envvars
 sudo sed -e "s?APACHE_RUN_GROUP=www-data?APACHE_RUN_GROUP=$USER?g" --in-place /etc/apache2/envvars
 sudo service apache2 stop; sudo rm -rf /var/lock/apache2; sudo service apache2 start
-
-# Link /usr/bin/perl to the current perlbrew perl so that the Bugzilla cgi scripts will work properly
-# PERLBREW_ROOT and PERLBREW_PERL are set by the perlbrew binary when switch perl versions
-echo "== Fixing Perl"
-sudo mv /usr/bin/perl /usr/bin/perl.bak
-sudo ln -s $PERLBREW_ROOT/perls/$PERLBREW_PERL/bin/perl /usr/bin/perl
-
-# We will be using SQLite for our database backend
-cpanm --quiet --notest DBD::SQLite
 
 # This is needed for the extended test suite
 cpanm --quiet --notest Test::WWW::Selenium
@@ -97,6 +119,7 @@ perl checksetup.pl qa/config/checksetup_answers.txt
 perl checksetup.pl qa/config/checksetup_answers.txt
 
 # Create test data in the SQLite database for use by the extended test suite
+echo "== Generating test data"
 cd $TRAVIS_BUILD_DIR/qa/config
 perl -Mlib=$TRAVIS_BUILD_DIR/lib generate_test_data.pl
 cd $TRAVIS_BUILD_DIR/qa/t
@@ -116,7 +139,7 @@ if [ "$TEST_SUITE" = "selenium" ]; then
     sleep 5
 
     echo "== Running Selenium UI tests"
-    perl -Mlib=$TRAVIS_BUILD_DIR/lib /usr/bin/prove -v -j4 -I$TRAVIS_BUILD_DIR/lib test_*.t
+    perl -Mlib=$TRAVIS_BUILD_DIR/lib /usr/bin/prove -v -j2 -I$TRAVIS_BUILD_DIR/lib test_*.t
     exit $?
 fi
 
